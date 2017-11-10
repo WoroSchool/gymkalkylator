d873b6b3b08757eea46ebfcd7bc891e8124c7fb0
WoroSchool <33526356+WoroSchool@users.noreply.github.com>
2017-11-10 16:57:28 +0200

    Update gymkalkylatortest.py

--- gymkalkylatortest.py
+++ gymkalkylatortest.py
@@ -1,21 +1,24 @@
-#Test till programmet gymkalkylator
+#Räkna plattor för stången
 def totalvikt(x):
 	plattorvikt = vikt - 20 
-	print(plattorvikt)
+	print(plattorvikt,'kg')
 	return plattorvikt
 
 def antalplattor(x):
 	plattorvikt = totalvikt(vikt)
-	while plattorvikt > 50:
+	while plattorvikt >= 50:
 		antal25 = plattorvikt // int(vikter[0])
-		print(antal25)
+		print(antal25,'st 25kg plattor')
 		return antal25
-		if plattorvikt < 50:
-			plattorvikt = plattorvikt // vikter[1]
-			print(plattorvikt)
-		elif plattorvikt < 40:
-			print()
-
+		if plattorvikt - antal25 * int(vikter[0]) < 50:
+			antal20 = (plattorvikt - antal25 * int(vikter[0])) // int(vikter[1])
+			print(antal20,'st 20kg plattor')
+			return antal20
+		elif plattorvikt - antal25 * int(vikter[0]) - antal20 * int(vikter[1]) < 40:
+			antal15 = (antal25 * int(vikter[0]) - antal20 * int(vikter[1])) // int(vikter[2])
+			print(antal15,'st 15kg plattor')
+			return antal15
+		elif plattorvikt - antal25 * int(vikter[0]) - antal20 * int(vikter[1]) < 40:
 
 
 vikter = [25,20,15,10,5,2.5,1.25]
@@ -24,6 +27,4 @@ vikter = [25,20,15,10,5,2.5,1.25]
 print (vikter,'plattor')
 
 vikt = int(input('Hur mycket vikt vill du ha?: '))
-
-totalvikt(vikt)
 antalplattor(totalvikt)
