9a003d5ee1eb9d3d467512e7b5567edf937c4170
WoroSchool <33526356+WoroSchool@users.noreply.github.com>
2017-11-10 17:00:57 +0200

    Update gymkalkylatortest.py

--- gymkalkylatortest.py
+++ gymkalkylatortest.py
@@ -6,19 +6,20 @@ def totalvikt(x):
 
 def antalplattor(x):
 	plattorvikt = totalvikt(vikt)
-	while plattorvikt >= 50:
+	if plattorvikt >= 50:
 		antal25 = plattorvikt // int(vikter[0])
 		print(antal25,'st 25kg plattor')
 		return antal25
-		if plattorvikt - antal25 * int(vikter[0]) < 50:
-			antal20 = (plattorvikt - antal25 * int(vikter[0])) // int(vikter[1])
-			print(antal20,'st 20kg plattor')
-			return antal20
-		elif plattorvikt - antal25 * int(vikter[0]) - antal20 * int(vikter[1]) < 40:
-			antal15 = (antal25 * int(vikter[0]) - antal20 * int(vikter[1])) // int(vikter[2])
-			print(antal15,'st 15kg plattor')
-			return antal15
-		elif plattorvikt - antal25 * int(vikter[0]) - antal20 * int(vikter[1]) < 40:
+	elif plattorvikt - antal25 * int(vikter[0]) < 50:
+		antal20 = (plattorvikt - antal25 * int(vikter[0])) // int(vikter[1])
+		print(antal20,'st 20kg plattor')
+		return antal20
+	elif plattorvikt - antal25 * int(vikter[0]) - antal20 * int(vikter[1]) < 40:
+		antal15 = (antal25 * int(vikter[0]) - antal20 * int(vikter[1])) // int(vikter[2])
+		print(antal15,'st 15kg plattor')
+		return antal15
+	elif plattorvikt - antal25 * int(vikter[0]) - antal20 * int(vikter[1]) > 40:
+		antal10 = (plattorvikt - antal25 * int(vikter[0]) - antal20 * int(vikter[1]) - antal15 * int(vikter[2]))
 
 
 vikter = [25,20,15,10,5,2.5,1.25]
